d244eb1fe278628df491cdfe4942f35f3e5b5626
nefoyoch <92327297+nefoyoch@users.noreply.github.com>
2021-11-13 21:43:54 +0530

    Add files for pagination assignment

---
+++ JS/index.js
@@ -0,0 +1,622 @@
+
+var tableData = [{
+    'first_name': 'Russell',
+    'last_name': 'Wilson',
+    'rank': '1',
+},
+{
+    'first_name': 'Matt',
+    'last_name': 'Hasselbeck',
+    'rank': '2',
+},
+{
+    'first_name': 'Jim',
+    'last_name': 'Zorn',
+    'rank': '3',
+},
+{
+    'first_name': 'Brady',
+    'last_name': 'Quinn',
+    'rank': '4',
+},
+{
+    'first_name': 'Charly',
+    'last_name': 'Whitehurst',
+    'rank': '5',
+},
+{
+    'first_name': 'Duane',
+    'last_name': 'Devine',
+    'rank': '6',
+},
+{
+    'first_name': 'Tom',
+    'last_name': 'Brady',
+    'rank': '7',
+},
+{
+    'first_name': 'Arron',
+    'last_name': 'Rogers',
+    'rank': '8',
+},
+{
+    'first_name': 'Patrick',
+    'last_name': 'Mahoms',
+    'rank': '9',
+},
+{
+    'first_name': 'Gardner',
+    'last_name': 'Minshew',
+    'rank': '10',
+},
+{
+    'first_name': 'Andrew',
+    'last_name': 'Luck',
+    'rank': '11',
+},
+{
+    'first_name': 'Josh',
+    'last_name': 'Gordon',
+    'rank': '12',
+},
+{
+    'first_name': 'Drew',
+    'last_name': 'Brees',
+    'rank': '13',
+},
+{
+    'first_name': 'Cam',
+    'last_name': 'Newton',
+    'rank': '14',
+},
+{
+    'first_name': 'Joe',
+    'last_name': 'Montana',
+    'rank': '15',
+},
+{
+    'first_name': 'Steve',
+    'last_name': 'Young',
+    'rank': '16',
+},
+{
+    'first_name': 'Dan',
+    'last_name': 'Jones',
+    'rank': '17',
+},
+{
+    'first_name': 'Eli',
+    'last_name': 'Manning',
+    'rank': '18',
+},
+{
+    'first_name': 'Marus',
+    'last_name': 'Marriota',
+    'rank': '19',
+},
+{
+    'first_name': 'Ryan',
+    'last_name': 'Fitzpatrick',
+    'rank': '20',
+},
+{
+    'first_name': 'Chad',
+    'last_name': 'Pennington',
+    'rank': '21',
+},
+{
+    'first_name': 'John',
+    'last_name': 'Doe',
+    'rank': '22',
+},
+{
+    'first_name': 'Russell',
+    'last_name': 'Wilson',
+    'rank': '23',
+},
+{
+    'first_name': 'Matt',
+    'last_name': 'Hasselbeck',
+    'rank': '24',
+},
+{
+    'first_name': 'Jim',
+    'last_name': 'Zorn',
+    'rank': '25',
+},
+{
+    'first_name': 'Brady',
+    'last_name': 'Quinn',
+    'rank': '26',
+},
+{
+    'first_name': 'Charly',
+    'last_name': 'Whitehurst',
+    'rank': '27',
+},
+{
+    'first_name': 'Duane',
+    'last_name': 'Devine',
+    'rank': '28',
+},
+{
+    'first_name': 'Tom',
+    'last_name': 'Brady',
+    'rank': '29',
+},
+{
+    'first_name': 'Arron',
+    'last_name': 'Rogers',
+    'rank': '30',
+},
+{
+    'first_name': 'Patrick',
+    'last_name': 'Mahoms',
+    'rank': '31',
+},
+{
+    'first_name': 'Gardner',
+    'last_name': 'Minshew',
+    'rank': '32',
+},
+{
+    'first_name': 'Andrew',
+    'last_name': 'Luck',
+    'rank': '33',
+},
+{
+    'first_name': 'Josh',
+    'last_name': 'Gordon',
+    'rank': '34',
+},
+{
+    'first_name': 'Drew',
+    'last_name': 'Brees',
+    'rank': '35',
+},
+{
+    'first_name': 'Cam',
+    'last_name': 'Newton',
+    'rank': '36',
+},
+{
+    'first_name': 'Joe',
+    'last_name': 'Montana',
+    'rank': '37',
+},
+{
+    'first_name': 'Steve',
+    'last_name': 'Young',
+    'rank': '38',
+},
+{
+    'first_name': 'Dan',
+    'last_name': 'Jones',
+    'rank': '39',
+},
+{
+    'first_name': 'Eli',
+    'last_name': 'Manning',
+    'rank': '40',
+},
+{
+    'first_name': 'Marus',
+    'last_name': 'Marriota',
+    'rank': '41',
+},
+{
+    'first_name': 'Ryan',
+    'last_name': 'Fitzpatrick',
+    'rank': '42',
+},
+{
+    'first_name': 'Chad',
+    'last_name': 'Pennington',
+    'rank': '43',
+},
+{
+    'first_name': 'John',
+    'last_name': 'Doe',
+    'rank': '44',
+},
+{
+    'first_name': 'Russell',
+    'last_name': 'Wilson',
+    'rank': '45',
+},
+{
+    'first_name': 'Matt',
+    'last_name': 'Hasselbeck',
+    'rank': '46',
+},
+{
+    'first_name': 'Jim',
+    'last_name': 'Zorn',
+    'rank': '47',
+},
+{
+    'first_name': 'Brady',
+    'last_name': 'Quinn',
+    'rank': '48',
+},
+{
+    'first_name': 'Charly',
+    'last_name': 'Whitehurst',
+    'rank': '49',
+},
+{
+    'first_name': 'Duane',
+    'last_name': 'Devine',
+    'rank': '50',
+},
+{
+    'first_name': 'Tom',
+    'last_name': 'Brady',
+    'rank': '51',
+},
+{
+    'first_name': 'Arron',
+    'last_name': 'Rogers',
+    'rank': '52',
+},
+{
+    'first_name': 'Patrick',
+    'last_name': 'Mahoms',
+    'rank': '53',
+},
+{
+    'first_name': 'Gardner',
+    'last_name': 'Minshew',
+    'rank': '54',
+},
+{
+    'first_name': 'Andrew',
+    'last_name': 'Luck',
+    'rank': '55',
+},
+{
+    'first_name': 'Josh',
+    'last_name': 'Gordon',
+    'rank': '56',
+},
+{
+    'first_name': 'Drew',
+    'last_name': 'Brees',
+    'rank': '57',
+},
+{
+    'first_name': 'Cam',
+    'last_name': 'Newton',
+    'rank': '58',
+},
+{
+    'first_name': 'Joe',
+    'last_name': 'Montana',
+    'rank': '59',
+},
+{
+    'first_name': 'Steve',
+    'last_name': 'Young',
+    'rank': '60',
+},
+{
+    'first_name': 'Dan',
+    'last_name': 'Jones',
+    'rank': '61',
+},
+{
+    'first_name': 'Eli',
+    'last_name': 'Manning',
+    'rank': '62',
+},
+{
+    'first_name': 'Marus',
+    'last_name': 'Marriota',
+    'rank': '63',
+},
+{
+    'first_name': 'Ryan',
+    'last_name': 'Fitzpatrick',
+    'rank': '64',
+},
+{
+    'first_name': 'Chad',
+    'last_name': 'Pennington',
+    'rank': '65',
+},
+{
+    'first_name': 'John',
+    'last_name': 'Doe',
+    'rank': '66',
+}, {
+    'first_name': 'Russell',
+    'last_name': 'Wilson',
+    'rank': '67',
+},
+{
+    'first_name': 'Matt',
+    'last_name': 'Hasselbeck',
+    'rank': '68',
+},
+{
+    'first_name': 'Jim',
+    'last_name': 'Zorn',
+    'rank': '69',
+},
+{
+    'first_name': 'Brady',
+    'last_name': 'Quinn',
+    'rank': '70',
+},
+{
+    'first_name': 'Charly',
+    'last_name': 'Whitehurst',
+    'rank': '71',
+},
+{
+    'first_name': 'Duane',
+    'last_name': 'Devine',
+    'rank': '72',
+},
+{
+    'first_name': 'Tom',
+    'last_name': 'Brady',
+    'rank': '73',
+},
+{
+    'first_name': 'Arron',
+    'last_name': 'Rogers',
+    'rank': '74',
+},
+{
+    'first_name': 'Patrick',
+    'last_name': 'Mahoms',
+    'rank': '75',
+},
+{
+    'first_name': 'Gardner',
+    'last_name': 'Minshew',
+    'rank': '76',
+},
+{
+    'first_name': 'Andrew',
+    'last_name': 'Luck',
+    'rank': '77',
+},
+{
+    'first_name': 'Josh',
+    'last_name': 'Gordon',
+    'rank': '78',
+},
+{
+    'first_name': 'Drew',
+    'last_name': 'Brees',
+    'rank': '79',
+},
+{
+    'first_name': 'Cam',
+    'last_name': 'Newton',
+    'rank': '80',
+},
+{
+    'first_name': 'Joe',
+    'last_name': 'Montana',
+    'rank': '81',
+},
+{
+    'first_name': 'Steve',
+    'last_name': 'Young',
+    'rank': '82',
+},
+{
+    'first_name': 'Dan',
+    'last_name': 'Jones',
+    'rank': '83',
+},
+{
+    'first_name': 'Eli',
+    'last_name': 'Manning',
+    'rank': '84',
+},
+{
+    'first_name': 'Marus',
+    'last_name': 'Marriota',
+    'rank': '85',
+},
+{
+    'first_name': 'Ryan',
+    'last_name': 'Fitzpatrick',
+    'rank': '86',
+},
+{
+    'first_name': 'Chad',
+    'last_name': 'Pennington',
+    'rank': '87',
+},
+{
+    'first_name': 'John',
+    'last_name': 'Doe',
+    'rank': '88',
+},
+{
+    'first_name': 'Patrick',
+    'last_name': 'Mahoms',
+    'rank': '89',
+},
+{
+    'first_name': 'Gardner',
+    'last_name': 'Minshew',
+    'rank': '90',
+},
+{
+    'first_name': 'Andrew',
+    'last_name': 'Luck',
+    'rank': '91',
+},
+{
+    'first_name': 'Josh',
+    'last_name': 'Gordon',
+    'rank': '92',
+},
+{
+    'first_name': 'Drew',
+    'last_name': 'Brees',
+    'rank': '93',
+},
+{
+    'first_name': 'Cam',
+    'last_name': 'Newton',
+    'rank': '94',
+},
+{
+    'first_name': 'Joe',
+    'last_name': 'Montana',
+    'rank': '95',
+},
+{
+    'first_name': 'Steve',
+    'last_name': 'Young',
+    'rank': '96',
+},
+{
+    'first_name': 'Dan',
+    'last_name': 'Jones',
+    'rank': '97',
+},
+{
+    'first_name': 'Eli',
+    'last_name': 'Manning',
+    'rank': '98',
+},
+{
+    'first_name': 'Marus',
+    'last_name': 'Marriota',
+    'rank': '99',
+},
+{
+    'first_name': 'Cam',
+    'last_name': 'Newton',
+    'rank': '100',
+},
+
+]
+
+buildTable()
+
+function buildTable() {
+var table = $('#table-body');
+var myList = tableData;
+console.log(myList.length);
+for (var i = 0; i<myList.length; i++) {
+    // console.log(myList);
+    var row = `<tr>
+              <td>${myList[i].rank}</td>
+              <td>${myList[i].first_name}</td>
+              <td>${myList[i].last_name}</td>
+              `
+    table.append(row)
+}
+}
+
+    let tbody = document.querySelector('tbody');
+    let tr = tbody.getElementsByTagName('tr');
+    let select = document.querySelector('select');
+    let ul = document.querySelector('.pagination');
+
+    let arrayTr = [];
+    for (let i = 0; i < tr.length; i++) {
+        console.log(tr[i]);
+        arrayTr.push(tr[i]);
+    }
+
+    select.onchange = rowCount;
+
+    function rowCount(e){
+
+// this is for buttons which are coming extra
+let neil = ul.querySelectorAll('.list');
+neil.forEach(n=>n.remove());
+
+        let limit = parseInt(e.target.value);
+        // console.log(limit); // get value which is selected in dropdown in a page
+        displayPage(limit);
+    }           
+
+    function displayPage(limit){
+        tbody.innerHTML = '';
+        for (let i = 0; i < limit; i++) {
+            tbody.appendChild(arrayTr[i]);
+            
+        }
+        buttonGenerator(limit);
+    }
+    displayPage(10); // by default how many rows
+
+    function buttonGenerator(limit){
+        const nofTr = arrayTr.length;
+        // console.log(nofTr);
+        if(nofTr <= limit){
+            // for buttons
+            ul.style.display = 'none';
+        }
+        else{
+            ul.style.display = 'flex';
+
+            const nofPage = Math.ceil(nofTr / limit);
+            console.log(nofPage);
+            for(i=1; i<=nofPage; i++){
+                let li = document.createElement('li');
+                li.className = 'list';
+                let a = document.createElement('a');
+                a.href = '#';
+                a.setAttribute('data-page', i);
+                li.appendChild(a);
+                a.innerText = i;
+                ul.insertBefore(li, ul.querySelector('.next'));
+
+                a.onclick = e =>{
+                    let x = e.target.getAttribute('data-page');
+                    tbody.innerHTML = '';
+                    x--;
+                    let start = limit * x;
+                    let end = start + limit;
+                    let page = arrayTr.slice(start, end);
+                    // console.log(page);
+                    for (let i = 0; i < page.length; i++) {
+                        let item = page[i];
+                        tbody.appendChild(item);
+                    }
+                }
+            }
+        }
+
+// fnctionality to prev, next, start and end button
+        let z = 0;
+        function nextElement(){
+            if(this.id == 'next'){
+                z == arrayTr.length - limit ? (z = 0) : (z += limit); 
+            }
+            if(this.id == 'prev'){
+                z == 0 ? arrayTr.length - limit : (z -= limit); 
+            }
+            if(this.id == 'first'){
+                z = 0; 
+            }
+            if(this.id == 'last'){
+                z = arrayTr.length - limit;
+            }
+            console.log("dfdf"+ z)
+            tbody.innerHTML = '';
+            for(let c=z; c<z+limit; c++){
+                console.log("ccc"+ c)
+                tbody.appendChild(arrayTr[c]);
+            }
+        }
+
+        document.getElementById('prev').onclick = nextElement;
+        document.getElementById('next').onclick = nextElement;
+        document.getElementById('first').onclick = nextElement;
+        document.getElementById('last').onclick = nextElement;
+
+    }
+ 

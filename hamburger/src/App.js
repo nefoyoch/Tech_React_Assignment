e4f0479ecccf7fe86ab9bc07328edff388bef759
nefoyoch <ayush@unthinkable.co>
2021-11-26 12:45:55 +0530

    create button resusbale component

--- hamburger/src/App.js
+++ hamburger/src/App.js
@@ -1,11 +1,23 @@
-import { Layout } from "./components/UI/Layout";
+import { Navbar } from "./components/UI/Navbar";
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
 
 function App() {
   return (
+    <Router>
     <div className="App">
-    {/* <h1>sd</h1> */}
-    <Layout/>
+    <Navbar/>
+    <Routes>
+      <Route exact path = "/" element = {<Navbar/>}></Route>
+      {/* <Route exact path = "/about" element = {<About/>}></Route>
+      <Route exact path = "/contact" element = {<Contact/>}></Route>
+      <Route exact path = "/apicall" element = {<CallAPI/>}></Route>
+      <Route exact path = "/lifecycle" element = {<LifeCycle/>}></Route>
+      <Route exact path = "/formik" element = {<Signup/>}></Route> */}
+    </Routes>
+    
     </div>
+    </Router>
   );
 }
 
--- hamburger/src/components/UI/Navbar.js
+++ hamburger/src/components/UI/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import './index.css';
 
-export const Layout = () => {
+export const Navbar = () => {
     return (
         <div>
 {/* <!--Navbar--> */}

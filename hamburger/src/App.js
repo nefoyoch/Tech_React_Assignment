657dc3597fa31dd889eddbc7745c5e8fc829b717
nefoyoch <ayush@unthinkable.co>
2021-11-28 22:33:57 +0530

    get data with axios

--- hamburger/src/App.js
+++ hamburger/src/App.js
@@ -2,11 +2,12 @@ import { useState } from "react/cjs/react.development";
 import { Navbar } from "./components/UI/Navbar";
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import { AddUser } from "./components/Users/AddUser";
-import UserList from "./components/Users/UserList";
+// import UserList from "./components/Users/UserList";
 import { Home } from "./components/UI/Home";
+import CallAPI from "./components/Users/CallAPI";
 
 
-function App(props) {
+function App() {
 
   const [usersList, setUsersList] = useState([]);
 
@@ -20,15 +21,15 @@ const addUserHandler = (uname, uAge, udep) => {
     <Router>
     <div className="App">
     <Navbar/>
-      <UserList users={usersList}/>
+      {/* <UserList /> */}
     <Routes>
       <Route exact path = "/" element = {<Home/>}></Route>
-      <Route exact path = "/adduser" element = {<AddUser onAddUser={addUserHandler}/>}></Route>
-      {/* <Route exact path = "/about" element = {<About/>}></Route>
-      <Route exact path = "/contact" element = {<Contact/>}></Route>
-      <Route exact path = "/apicall" element = {<CallAPI/>}></Route>
-      <Route exact path = "/lifecycle" element = {<LifeCycle/>}></Route>
-      <Route exact path = "/formik" element = {<Signup/>}></Route> */}
+      <Route exact path = "/adduser" element = {<AddUser users={usersList} onAddUser={addUserHandler}/>}></Route>
+      <Route exact path = "/api" element = {<CallAPI/>}></Route>
+      {/* <Route exact path = "/contact" element = {<Contact/>}></Route> */}
+      {/* <Route exact path = "/apicall" element = {<CallAPI/>}></Route> */}
+      {/* <Route exact path = "/lifecycle" element = {<LifeCycle/>}></Route> */}
+      {/* <Route exact path = "/formik" element = {<Signup/>}></Route> */}
     </Routes>
     
     </div>

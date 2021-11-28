6795000c9a3742d19b5d00bf3a673476d4053cdd
nefoyoch <ayush@unthinkable.co>
2021-11-28 23:21:12 +0530

    add data for pagination

---
+++ hamburger/src/assets/library/data.js
@@ -0,0 +1,507 @@
+var tableData = [
+    {
+      first_name: "Russell",
+      last_name: "Wilson",
+      rank: "1",
+    },
+    {
+      first_name: "Matt",
+      last_name: "Hasselbeck",
+      rank: "2",
+    },
+    {
+      first_name: "Jim",
+      last_name: "Zorn",
+      rank: "3",
+    },
+    {
+      first_name: "Brady",
+      last_name: "Quinn",
+      rank: "4",
+    },
+    {
+      first_name: "Charly",
+      last_name: "Whitehurst",
+      rank: "5",
+    },
+    {
+      first_name: "Duane",
+      last_name: "Devine",
+      rank: "6",
+    },
+    {
+      first_name: "Tom",
+      last_name: "Brady",
+      rank: "7",
+    },
+    {
+      first_name: "Arron",
+      last_name: "Rogers",
+      rank: "8",
+    },
+    {
+      first_name: "Patrick",
+      last_name: "Mahoms",
+      rank: "9",
+    },
+    {
+      first_name: "Gardner",
+      last_name: "Minshew",
+      rank: "10",
+    },
+    {
+      first_name: "Andrew",
+      last_name: "Luck",
+      rank: "11",
+    },
+    {
+      first_name: "Josh",
+      last_name: "Gordon",
+      rank: "12",
+    },
+    {
+      first_name: "Drew",
+      last_name: "Brees",
+      rank: "13",
+    },
+    {
+      first_name: "Cam",
+      last_name: "Newton",
+      rank: "14",
+    },
+    {
+      first_name: "Joe",
+      last_name: "Montana",
+      rank: "15",
+    },
+    {
+      first_name: "Steve",
+      last_name: "Young",
+      rank: "16",
+    },
+    {
+      first_name: "Dan",
+      last_name: "Jones",
+      rank: "17",
+    },
+    {
+      first_name: "Eli",
+      last_name: "Manning",
+      rank: "18",
+    },
+    {
+      first_name: "Marus",
+      last_name: "Marriota",
+      rank: "19",
+    },
+    {
+      first_name: "Ryan",
+      last_name: "Fitzpatrick",
+      rank: "20",
+    },
+    {
+      first_name: "Chad",
+      last_name: "Pennington",
+      rank: "21",
+    },
+    {
+      first_name: "John",
+      last_name: "Doe",
+      rank: "22",
+    },
+    {
+      first_name: "Russell",
+      last_name: "Wilson",
+      rank: "23",
+    },
+    {
+      first_name: "Matt",
+      last_name: "Hasselbeck",
+      rank: "24",
+    },
+    {
+      first_name: "Jim",
+      last_name: "Zorn",
+      rank: "25",
+    },
+    {
+      first_name: "Brady",
+      last_name: "Quinn",
+      rank: "26",
+    },
+    {
+      first_name: "Charly",
+      last_name: "Whitehurst",
+      rank: "27",
+    },
+    {
+      first_name: "Duane",
+      last_name: "Devine",
+      rank: "28",
+    },
+    {
+      first_name: "Tom",
+      last_name: "Brady",
+      rank: "29",
+    },
+    {
+      first_name: "Arron",
+      last_name: "Rogers",
+      rank: "30",
+    },
+    {
+      first_name: "Patrick",
+      last_name: "Mahoms",
+      rank: "31",
+    },
+    {
+      first_name: "Gardner",
+      last_name: "Minshew",
+      rank: "32",
+    },
+    {
+      first_name: "Andrew",
+      last_name: "Luck",
+      rank: "33",
+    },
+    {
+      first_name: "Josh",
+      last_name: "Gordon",
+      rank: "34",
+    },
+    {
+      first_name: "Drew",
+      last_name: "Brees",
+      rank: "35",
+    },
+    {
+      first_name: "Cam",
+      last_name: "Newton",
+      rank: "36",
+    },
+    {
+      first_name: "Joe",
+      last_name: "Montana",
+      rank: "37",
+    },
+    {
+      first_name: "Steve",
+      last_name: "Young",
+      rank: "38",
+    },
+    {
+      first_name: "Dan",
+      last_name: "Jones",
+      rank: "39",
+    },
+    {
+      first_name: "Eli",
+      last_name: "Manning",
+      rank: "40",
+    },
+    {
+      first_name: "Marus",
+      last_name: "Marriota",
+      rank: "41",
+    },
+    {
+      first_name: "Ryan",
+      last_name: "Fitzpatrick",
+      rank: "42",
+    },
+    {
+      first_name: "Chad",
+      last_name: "Pennington",
+      rank: "43",
+    },
+    {
+      first_name: "John",
+      last_name: "Doe",
+      rank: "44",
+    },
+    {
+      first_name: "Russell",
+      last_name: "Wilson",
+      rank: "45",
+    },
+    {
+      first_name: "Matt",
+      last_name: "Hasselbeck",
+      rank: "46",
+    },
+    {
+      first_name: "Jim",
+      last_name: "Zorn",
+      rank: "47",
+    },
+    {
+      first_name: "Brady",
+      last_name: "Quinn",
+      rank: "48",
+    },
+    {
+      first_name: "Charly",
+      last_name: "Whitehurst",
+      rank: "49",
+    },
+    {
+      first_name: "Duane",
+      last_name: "Devine",
+      rank: "50",
+    },
+    {
+      first_name: "Tom",
+      last_name: "Brady",
+      rank: "51",
+    },
+    {
+      first_name: "Arron",
+      last_name: "Rogers",
+      rank: "52",
+    },
+    {
+      first_name: "Patrick",
+      last_name: "Mahoms",
+      rank: "53",
+    },
+    {
+      first_name: "Gardner",
+      last_name: "Minshew",
+      rank: "54",
+    },
+    {
+      first_name: "Andrew",
+      last_name: "Luck",
+      rank: "55",
+    },
+    {
+      first_name: "Josh",
+      last_name: "Gordon",
+      rank: "56",
+    },
+    {
+      first_name: "Drew",
+      last_name: "Brees",
+      rank: "57",
+    },
+    {
+      first_name: "Cam",
+      last_name: "Newton",
+      rank: "58",
+    },
+    {
+      first_name: "Joe",
+      last_name: "Montana",
+      rank: "59",
+    },
+    {
+      first_name: "Steve",
+      last_name: "Young",
+      rank: "60",
+    },
+    {
+      first_name: "Dan",
+      last_name: "Jones",
+      rank: "61",
+    },
+    {
+      first_name: "Eli",
+      last_name: "Manning",
+      rank: "62",
+    },
+    {
+      first_name: "Marus",
+      last_name: "Marriota",
+      rank: "63",
+    },
+    {
+      first_name: "Ryan",
+      last_name: "Fitzpatrick",
+      rank: "64",
+    },
+    {
+      first_name: "Chad",
+      last_name: "Pennington",
+      rank: "65",
+    },
+    {
+      first_name: "John",
+      last_name: "Doe",
+      rank: "66",
+    },
+    {
+      first_name: "Russell",
+      last_name: "Wilson",
+      rank: "67",
+    },
+    {
+      first_name: "Matt",
+      last_name: "Hasselbeck",
+      rank: "68",
+    },
+    {
+      first_name: "Jim",
+      last_name: "Zorn",
+      rank: "69",
+    },
+    {
+      first_name: "Brady",
+      last_name: "Quinn",
+      rank: "70",
+    },
+    {
+      first_name: "Charly",
+      last_name: "Whitehurst",
+      rank: "71",
+    },
+    {
+      first_name: "Duane",
+      last_name: "Devine",
+      rank: "72",
+    },
+    {
+      first_name: "Tom",
+      last_name: "Brady",
+      rank: "73",
+    },
+    {
+      first_name: "Arron",
+      last_name: "Rogers",
+      rank: "74",
+    },
+    {
+      first_name: "Patrick",
+      last_name: "Mahoms",
+      rank: "75",
+    },
+    {
+      first_name: "Gardner",
+      last_name: "Minshew",
+      rank: "76",
+    },
+    {
+      first_name: "Andrew",
+      last_name: "Luck",
+      rank: "77",
+    },
+    {
+      first_name: "Josh",
+      last_name: "Gordon",
+      rank: "78",
+    },
+    {
+      first_name: "Drew",
+      last_name: "Brees",
+      rank: "79",
+    },
+    {
+      first_name: "Cam",
+      last_name: "Newton",
+      rank: "80",
+    },
+    {
+      first_name: "Joe",
+      last_name: "Montana",
+      rank: "81",
+    },
+    {
+      first_name: "Steve",
+      last_name: "Young",
+      rank: "82",
+    },
+    {
+      first_name: "Dan",
+      last_name: "Jones",
+      rank: "83",
+    },
+    {
+      first_name: "Eli",
+      last_name: "Manning",
+      rank: "84",
+    },
+    {
+      first_name: "Marus",
+      last_name: "Marriota",
+      rank: "85",
+    },
+    {
+      first_name: "Ryan",
+      last_name: "Fitzpatrick",
+      rank: "86",
+    },
+    {
+      first_name: "Chad",
+      last_name: "Pennington",
+      rank: "87",
+    },
+    {
+      first_name: "John",
+      last_name: "Doe",
+      rank: "88",
+    },
+    {
+      first_name: "Patrick",
+      last_name: "Mahoms",
+      rank: "89",
+    },
+    {
+      first_name: "Gardner",
+      last_name: "Minshew",
+      rank: "90",
+    },
+    {
+      first_name: "Andrew",
+      last_name: "Luck",
+      rank: "91",
+    },
+    {
+      first_name: "Josh",
+      last_name: "Gordon",
+      rank: "92",
+    },
+    {
+      first_name: "Drew",
+      last_name: "Brees",
+      rank: "93",
+    },
+    {
+      first_name: "Cam",
+      last_name: "Newton",
+      rank: "94",
+    },
+    {
+      first_name: "Joe",
+      last_name: "Montana",
+      rank: "95",
+    },
+    {
+      first_name: "Steve",
+      last_name: "Young",
+      rank: "96",
+    },
+    {
+      first_name: "Dan",
+      last_name: "Jones",
+      rank: "97",
+    },
+    {
+      first_name: "Eli",
+      last_name: "Manning",
+      rank: "98",
+    },
+    {
+      first_name: "Marus",
+      last_name: "Marriota",
+      rank: "99",
+    },
+    {
+      first_name: "Cam",
+      last_name: "Newton",
+      rank: "100",
+    },
+
+  ];
+
+  export default function Data(){
+    return tableData;
+  };               
